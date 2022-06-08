b4753d83aa7a47155972f220db15f39c9a938ed1
Ed Adkins <ejmadkins@gmail.com>
2022-06-08 18:15:12 +0100

    add placeholder image and styles

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,8 +6,9 @@ const SearchResults = () => {
     <>
       <p>Search Results</p>
       <img
-        alt="search-result"
+        className="search-result"
         src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+        alt="search-result"
       />
     </>
   );
