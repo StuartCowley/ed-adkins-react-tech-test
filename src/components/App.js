41dc3d61aab0d80fefa0abb43ea9af998020a911
Ed Adkins <ejmadkins@gmail.com>
2022-06-08 17:05:41 +0100

    store state of search results

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search";
 import "../styles/app.css";
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState();
+  console.log(searchResults);
+
   return (
     <div className="app">
       <img
         className="nasa-logo"
         src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
         alt="A Nasa Logo"
-      ></img>
-      <Search />
+      />
+      <Search setSearchResults={setSearchResults} />
     </div>
   );
 };
