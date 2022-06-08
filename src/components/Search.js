d836de0bcf7b34d56af9df38f133a42a4561a833
Ed Adkins <ejmadkins@gmail.com>
2022-06-08 14:05:30 +0100

    add search input

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/search.css";
+
+const Search = () => {
+  return (
+    <>
+      <input className="image-search" type="text"></input>
+    </>
+  );
+};
+
+export default Search;
