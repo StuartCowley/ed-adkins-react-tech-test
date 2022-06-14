7f0f42e0f086c7fb884d2646df495557850c856f
Ed Adkins <ejmadkins@gmail.com>
2022-06-14 14:31:18 +0100

    fix issue with missing setLoading

--- src/components/App.js
+++ src/components/App.js
@@ -34,6 +34,7 @@ const App = () => {
         setSearch={setSearch}
         page={page}
         setPage={setPage}
+        setLoading={setLoading}
       />
       <SearchResults
         results={searchResults}
